96bf8c8b1a440b0f3f05add5ad5f6ee5d0f904c2
Drei08 <andrei_rheivjarth@hotmail.com>
2026-06-01 19:31:37 -0300

    CreatePost

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,69 @@
 import styles from './CreatePost.module.css';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+
+
 const CreatePost = () => {
+
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState(''); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Create Post</h1>
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p>Seja criativo e compartilhe suas ideias!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input type="text" 
+          name="title" 
+          required 
+          placeholder="Nome do Título..." 
+          onChange={(e) => setTitle(e.target.value)} 
+          value={title} />
+        </label>
+         <label>
+          <span>Url da Imagem:</span>
+          <input type="text" 
+          name="image" 
+          required 
+          placeholder="Insiera a Url da Imagem..." 
+          onChange={(e) => setImage(e.target.value)} 
+          value={image} />
+        </label>
+         <label>
+          <span>Conteúdo:</span>
+          <textarea name="body" 
+          required 
+          placeholder="Digite o conteúdo do post..." 
+          onChange={(e) => setBody(e.target.value)} 
+          value={body}>
+          </textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input type="text" 
+          name="tags" 
+          required 
+          placeholder="Insiera as Tags separadas com virgulas..." 
+          onChange={(e) => setTags(e.target.value)} 
+          value={tags} />
+        </label>
+        <button className="btn">Cadastrar</button>
+        {/* {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn" disabled>Cadastrando...</button>}
+        {/* Exibe mensagem de erro, se houver */}
+        {/* {error && <p className="error">{error}</p>} */} 
+      </form>
     </div>
   )
 }
