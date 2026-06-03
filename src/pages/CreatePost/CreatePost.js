7bb08c643fbadb31b1b5d20bdefb8bda934cb59f
Drei08 <andrei_rheivjarth@hotmail.com>
2026-06-02 22:54:45 -0300

    hook-data-useFetchDocuments

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -16,7 +16,7 @@
 
     const { user } = useAuthValue();
 
-    const {insertDocument, response} = useInsertDocument("posts");
+    const {insertDocument, response} = useInsertDocument("po");
 
     const navigate = useNavigate();
 
